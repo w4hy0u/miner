2472613e74fe42bd041356b2305f0a119ec62154
w4hy0u <42631570+w4hy0u@users.noreply.github.com>
2021-06-02 06:54:48 +0700

    Create Sugar.sh

---
+++ Sugar.sh
@@ -0,0 +1,5 @@
+#sugar
+sudo apt update
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz 
+tar xf cpuminer-opt-linux.tar.gz 
+./cpuminer-sse2 -a yespowersugar -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1qcx7tkltda5xdgxn4cvj6ns6ymw985ptltaz5me -t 4
